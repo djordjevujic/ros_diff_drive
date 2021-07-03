7f93242f356964c468a1b8d7ea6f84c0f57e6515
Djordje <djordjevujic@yahoo.com>
2021-07-03 14:35:51 +0200

    identation fix

--- scripts/SrvDynamicRec.py
+++ scripts/SrvDynamicRec.py
@@ -5,19 +5,24 @@ import rospy
 from dynamic_reconfigure.server import Server
 from ros_diff_drive.cfg import DynRecPIDConfig
 
-KP_ROT = float()
-KP_ROT = float()
-
 def callback(config, level):
-    global KP_ROT, KP_ROT
+  rospy.loginfo("""Reconfigure Request: {KP_ROT}, {TI_ROT}, {TD_ROT} {UI_LIMIT}\ 
+        """.format(**config))
 
-    rospy.loginfo("""Reconfigure Request: {KP_ROT}, {KI_ROT},\ 
-          """.format(**config))
+  return config
 
-    return config
+## @brief  Reads rotation PID parameters
+## @return KP, KI, KD respectively
+#def get_rot_dyn_conf():
+  #KP_ROT = rospy.get_param('pid_dynamic_reconfigure/KP_ROT')
+  #KI_ROT = rospy.get_param('pid_dynamic_reconfigure/TI_ROT')
+  #KD_ROT = rospy.get_param('pid_dynamic_reconfigure/TD_ROT')
+  #INT_LIMIT = rospy.get_param('pid_dynamic_reconfigure/UI_LIMIT')
+
+  #return KP_ROT, KI_ROT, KD_ROT, INT_LIMIT
 
 if __name__ == "__main__":
-    rospy.init_node("dyn_pid_params", anonymous = False)
+  rospy.init_node("dyn_pid_params", anonymous = False)
 
-    srv = Server(DynRecPIDConfig, callback)
-    rospy.spin()
+  srv = Server(DynRecPIDConfig, callback)
+  rospy.spin()
