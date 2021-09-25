9e935143d5cceb4b64559b1dcd4050626376a8ff
Djordje <djordjevujic@yahoo.com>
2021-09-25 10:52:49 +0200

    Doxygen comments updated, debug and config stuff moved outside

--- scripts/move_to_point.py
+++ scripts/move_to_point.py
@@ -1,3 +1,7 @@
+"""
+Implements differential drive robot control
+"""
+
 #!/usr/bin/env python
 
 # TODO:
@@ -5,68 +9,98 @@
 
 import rospy
 from tf.transformations import euler_from_quaternion
-from math import atan2, sqrt, pow, pi, degrees
+from math import atan2, sqrt, degrees
 from fsm import FsmState, FsmStates, FsmRobot
 from regulator import Regulator
 from ros_diff_drive.cfg import DynRecPIDConfig
 from dynamic_reconfigure.server import Server
 from geometry_msgs.msg import Twist, Point, Pose2D
 from nav_msgs.msg import Odometry
-from std_msgs.msg import Float32, Int32
+from config import *
+from debug import *
 
-# Controller frequency in Hertz
-controller_freq = 50.0
-
+## Controller's period in seconds
 T = 1.0 / controller_freq
 
-# PID Parameters initialization
-KP_ROT = 0.0
-TI_ROT = 0.000001
-TD_ROT = 0.0
-INT_LIMIT_ROT = 0.0
-P_ANG_DST = 0.0
-P_ANG_THT = 0.0
+## @defgroup xyInit Values updated during the callback of the odometry
+## @{
 
-KP_FWD = 0.0
-TI_FWD = 0.000001
-TD_FWD = 0.0
-P_FWD_DST = 0.0
-P_FWD_CUR = 0.0
-INT_LIMIT_FWD = 0.0
+## Initial x coordinate from the start of moving forward
+xInitial = 0.0
 
-# Rotation speed limit
-rot_speed_limit = 2.0
+## Initial y coordinate from the start of moving forward
+yInitial = 0.0
+## @}
 
-# Speed limit of moving forward
-fwd_speed_limit = 0.7
-
-# Angle error tolerance in degrees
-angle_err_tolerance = 0.04
-
-# Distance error tolerance
-dist_err_tolerance = 0.005
-
-# Values updated during the callback
+## @defgroup callbackPos Values updated during the callback of the odometry
+## These values are updated during \ref position_callback
+## @{
 x = 0.0
 y = 0.0
 theta = 0.0
+## @}
 
-# Goal input coordinates
+## Goal input coordinates
+## Updated in \ref goal_position_callback
 goal = Point(0, 0, 0)
 
-# TODO remove later - debug var
-GOAL_THETA = 0.0
-GOAL_DIST = 0.0
+## @defgroup debugVar Variables used for debugging purposes
+## If debugging only rotation/forward, they receive the value
+## from the dynamic reconfigure.
+## @{
 
-# Goal coordinates that are under processing
+## Goal angle
+GOAL_THETA = 0.0
+
+## Goal distance
+GOAL_DIST = 0.0
+## @}
+
+## @brief Goal coordinates that are under processing
 active_goal = Point(0, 0, 0)
 
-## @brief  Calculate rotation direction
-##         Positive rotation direction - clockwise
+# Rotation values
+## Filtered value of the goal angle
+angle_to_goal_filt = 0.0
+
+## Filtered value of the current angle
+theta_filt = 0.0
+
+## Constant for indicating positive angle
+POSITIVE = True
+
+## Constant for indicating negative angle
+NEGATIVE = False
+
+## Variable indicating signess of the current iteration angle: \ref POSITIVE or \ref NEGATIVE
+theta_sign = POSITIVE
+
+## Variable indicating signess of the angle from the previous iteration: \ref POSITIVE or \ref NEGATIVE
+theta_sign_prev = POSITIVE
+
+# Moving forward values
+## Calculated goal distance
+goal_distance = 0.0
+
+## Filtered value of the goal distance
+goal_distance_filt = 0.0
+
+## Filtered value of the desired distance
+dist_filt = 0.0
+
+## Indicates if moving forward started or not
+move_fwd_started = False
+
+## Used for correcting angle error which accumulates while moving forward
+angle_to_goal_fwd = 0.0
+
+
+## @brief  Calculate rotation direction \n
+##         Positive rotation direction - clockwise \n
 ##         Negative rotation direction - counter clockwise
 ## @param  target_angle  Angle to be reached [degrees]
 ## @param  current_angle Current angle of the robot [degrees]
-## @return error Error including direction [degrees]
+## @return Error including direction [degrees]
 def angle_error_calc(target_angle, current_angle):
 
   # Normalize angles
@@ -85,6 +119,11 @@ def angle_error_calc(target_angle, current_angle):
 
   return error
 
+## @brief  Dynamic reconfigure callback function
+##
+## @param  config Contains all dynamic parameters
+## @param  level  Not used
+## @return config
 def dyn_reconf_callback(config, level):
   global KP_ROT, TI_ROT, TD_ROT, INT_LIMIT_ROT, P_ANG_DST, P_ANG_THT, \
          KP_FWD, TI_FWD, TD_FWD, P_FWD_DST, P_FWD_CUR, INT_LIMIT_FWD
@@ -122,11 +161,10 @@ def position_callback(msg):
   # Convert theta to degrees
   theta = degrees(theta)
 
-# TODO Move later
-xInitial = 0.0
-yInitial = 0.0
+## @brief Callback function for processing goal values
+## @param msg Message to be processed - contains desired goal position
 def goal_position_callback(msg):
-  global goal, GOAL_THETA, GOAL_DIST, xInitial, yInitial # TODO: Remove later
+  global goal, GOAL_THETA, GOAL_DIST, xInitial, yInitial
 
   if msg.x != goal.x or msg.y != goal.y or msg.theta != GOAL_THETA:
     goal.x = msg.x
@@ -137,6 +175,9 @@ def goal_position_callback(msg):
     yInitial = y
     print("/target_position/position Goal: ({}, {})".format(goal.x, goal.y))
 
+## @brief Idle function of the robot state machine
+##
+## In this state robot is waiting for the new command to arrive.
 def idle():
   global robot_fsm, StateRotation, goal, active_goal
 
@@ -149,21 +190,21 @@ def idle():
 
     # Switch to rotation state
     robot_fsm.switch_state(StateRotation)
-    #robot_fsm.switch_state(StateForward) # TODO: Delete
+    #robot_fsm.switch_state(StateForward) # Can be used for debugging purposes
 
     rospy.logdebug("New active goal: (%d,%d)", active_goal.x, active_goal.y)
     print("[IDL] New goal: ({},{})".format(active_goal.x, active_goal.y))
 
-# Filtered values used for the rotation
-angle_to_goal_filt = 0.0
-theta_filt = 0.0
-POSITIVE = True
-NEGATIVE = False
-theta_sign = POSITIVE
-theta_sign_prev = POSITIVE
-
+## @brief Rotation function of the robot state machine
+##
+## This state controls robot rotation. It filters current angle and desired angle values, calculates an error in degrees
+## and generates desired rotation speed calculated in PID routine.
+## Desired rotation speed is then published to the velocity publisher \ref pub_cmd_vel.
 def rotate():
   global robot_fsm, active_goal, theta, goal, angle_to_goal_filt, theta_filt, theta_sign, theta_sign_prev
+
+  # Used for the velocity command storage
+  velocity = Twist()
 
   # Check signess of the current angle
   if theta > 0.0:
@@ -201,7 +242,7 @@ def rotate():
   else:
     velocity.angular.z = 0
     robot_fsm.switch_state(StateForward)
-    #robot_fsm.switch_state(StateIdle) #TODO: Remove
+    #robot_fsm.switch_state(StateIdle) # Can be used for debugging purposes
 
   # Publish command
   pub_cmd_vel.publish(velocity)
@@ -217,21 +258,23 @@ def rotate():
   print("[ROT] Angle to goal: {}, theta: {}".format(angle_to_goal, theta))
   #print("[ROT] Angle error: {}".format(angle_error))
 
-goal_distance = 0.0
-goal_distance_filt = 0.0
-dist_filt = 0.0
-move_fwd_started = False
-angle_to_goal_fwd = 0.0
-
+## @brief State machine functionality for moving forward
+##
+## This state controls robot when moving forward. It filters current distance and desired distance values, calculates an error,
+## and generates desired linear speed calculated in PID routine.
+## Desired rotation speed is then published to the velocity publisher \ref pub_cmd_vel.
 def forward():
   global robot_fsm, active_goal, goal_distance_filt, goal_distance, dist_filt, move_fwd_started, xInitial, yInitial, angle_to_goal_fwd
 
+  # Used for the velocity command storage
+  velocity = Twist()
+
   if move_fwd_started == False:
     # Calculate current distance to the point
-    xInitial = x # TODO: Uncomment
-    yInitial = y # TODO: Uncomment
+    xInitial = x
+    yInitial = y
     goal_distance = sqrt((active_goal.x - x)**2 + (active_goal.y - y)**2)
-    #goal_distance = GOAL_DIST # TODO: Comment out later, and uncomment previous line
+    #goal_distance = GOAL_DIST # This line is only for debugging purposes
     move_fwd_started = True
 
   # Update a distance from the initial point
@@ -281,62 +324,52 @@ def forward():
 
   print("[FWD] Point({}, {}) Dist to goal: {}, Current: {}".format(x, y, goal_distance, distance))
 
-# Initialize goal destination subscriber
+## Goal destination subscriber
 sub_goal_position = rospy.Subscriber("/target_position/position", Pose2D, goal_position_callback)
 
-# Initialize odometry subscriber
+## Odometry (current position) subscriber
 sub_odom = rospy.Subscriber("/m2xr_diff_drive_controller/odom", Odometry, position_callback)
 
-# Initialize velocity command publisher
+##  @brief cmd_vel publisher
+##  Used to publish desired velocity to the next layer of the control.
 pub_cmd_vel = rospy.Publisher("/m2xr_diff_drive_controller/cmd_vel", Twist, queue_size=1)
-
-# Debug publishers
-#   Rotation
-pub_dbg_angle_err = rospy.Publisher("debug/angle_err", Float32, queue_size = 5)
-pub_dbg_theta = rospy.Publisher("debug/theta", Float32, queue_size=5)
-pub_dbg_theta_filtr = rospy.Publisher("debug/theta_filtr", Float32, queue_size=5)
-pub_dbg_ang_to_goal = rospy.Publisher("debug/angle_to_goal", Float32, queue_size=5)
-pub_dbg_ang_to_goal_filtr = rospy.Publisher("debug/angle_to_goal_filtr", Float32, queue_size=5)
-pub_dbg_rot = rospy.Publisher("debug/rot_vel", Float32, queue_size=5)
-#   Moving forward
-pub_dbg_distance = rospy.Publisher("/debug/distance", Float32, queue_size = 5)
-pub_dbg_distance_filtr = rospy.Publisher("/debug/dist_filtr", Float32, queue_size = 5)
-pub_dbg_dist_to_goal = rospy.Publisher("/debug/dist_to_goal", Float32, queue_size = 5)
-pub_dbg_dist_to_goal_filtr = rospy.Publisher("/debug/dist_to_goal_filtr", Float32, queue_size = 5)
-pub_dbg_fwd = rospy.Publisher("/debug/dist_velocity", Float32, queue_size = 5)
-pub_dbg_fwd_rot = rospy.Publisher("/debug/fwd_rot", Float32, queue_size = 5)
-pub_dbg_fwd_rot_vel = rospy.Publisher("/debug/fwd_rot_vel", Float32, queue_size = 5)
-
-velocity = Twist()
 
 rospy.init_node("speed_controller")
 
+## Initialization of the speed_controller node
 r = rospy.Rate(controller_freq)
 
-# Define Robot Finite State Machine states
+## FSM idle state definition
 StateIdle = FsmState(FsmStates.Idle, idle)
+
+## FSM rotation state definition
 StateRotation = FsmState(FsmStates.Rotating, rotate)
+
+## FSM moving forward state definition
 StateForward = FsmState(FsmStates.Forward, forward)
 
+## List of permitted states
 StatesList = [StateIdle, StateRotation, StateForward]
-#StatesList = [StateRotation] # TODO: Remove
+#StatesList = [StateRotation] # Used only for debugging purposes
 
-# Initialize the state machine
+## FSM Initialization
 robot_fsm = FsmRobot("M2XR", StatesList, StatesList[0])
 
-# Initialize dynamic reconfigure server
+## Dynamic reconfigure server initialization
 srv__dyn_reconf = Server(DynRecPIDConfig, dyn_reconf_callback)
 
-# Initialize PID regulators
+## Normal rotation PID regulator initialization
 rot_pid = Regulator(KP_ROT, TI_ROT, TD_ROT, T, rot_speed_limit, INT_LIMIT_ROT)
+
+## Moving forward PID regulator initialization
 fwd_pid = Regulator(KP_FWD, TI_FWD, TD_FWD, T, fwd_speed_limit, INT_LIMIT_FWD)
+
+## Rotation while moving forward PID regulator initialization
 fwd_pid_rot = Regulator(KP_ROT, TI_ROT, TD_ROT, T, rot_speed_limit, INT_LIMIT_ROT)
 
 while not rospy.is_shutdown():
+  
   # Update PID parameters
-
-  #  print("KP_ROT: "+ str(KP_ROT))
-
   rot_pid.update_params(KP_ROT, TI_ROT, TD_ROT, INT_LIMIT_ROT)
   fwd_pid.update_params(KP_FWD, TI_FWD, TD_FWD, INT_LIMIT_FWD)
   fwd_pid_rot.update_params(KP_ROT, TI_ROT, TD_ROT, INT_LIMIT_ROT)
@@ -344,5 +377,5 @@ while not rospy.is_shutdown():
   # Execute state from the FSM
   robot_fsm.execute()
 
-  # Sleep some time...
+  # Sleep for some time...
   r.sleep()
